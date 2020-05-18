a190f99c6df91eee4398a6ef6cf98afca6c85e45
fawwazmayda <maydafawwaz@gmail.com>
2020-05-18 22:00:48 +0800

    Styling Info page

--- FirstSwiftUI/AboutView.swift
+++ FirstSwiftUI/AboutView.swift
@@ -9,11 +9,37 @@
 import SwiftUI
 
 struct AboutView: View {
+    
+    let beige = Color(red: 1.0, green: 214.0/255.0, blue: 179.0/255.0)
+    struct HeadingStyle:ViewModifier {
+        func body(content: Content) -> some View {
+            content.foregroundColor(.black)
+                .font(.custom("Arial Rounded MT Bold", size: 30.0))
+                .padding(.top, 20.0)
+                .padding(.bottom, 20.0)
+        }
+    }
+    
+    struct BodyStyle:ViewModifier {
+        func body(content: Content) -> some View {
+            content.foregroundColor(.black)
+                .font(.custom("Arial Rounded MT Bold", size: 16.0))
+                .padding(.bottom, 20.0)
+                .padding(.leading, 60.0)
+                .padding(.trailing, 60.0)
+        }
+    }
+    
+    
     var body: some View {
-        VStack {
-            Text(" 🎯 Bulls Eye 🎯")
-            Text(" This is a bullseye game")
-        }.navigationBarTitle(Text("About Bullseye"))
+        Group {
+            VStack {
+                Text(" 🎯 Bulls Eye 🎯").modifier(HeadingStyle())
+                Text(" This is a bullseye game where you can earn fame by dragging a slider").modifier(BodyStyle())
+                Text("Your goal is to place slider as close as to target value. The closer you are, the more points you score").modifier(BodyStyle())
+                Text("Enjoy").modifier(BodyStyle())
+            }.navigationBarTitle(Text("About Bullseye")).background(beige)
+        }.background(Image("Background"))
     }
 }
 
